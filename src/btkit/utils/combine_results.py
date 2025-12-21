b1fcc715059637c5f8fdf564987db566192b74fa
Jack Rochester <jrochester@jacks-mbp.lan>
2025-12-21 10:47:34 -0500

    explicitly add schema to metadata dataframe in combine_results

--- src/btkit/utils/combine_results.py
+++ src/btkit/utils/combine_results.py
@@ -17,8 +17,19 @@ def combine_backtest_results(backtest_logs_path: str, output_db_path: str):
     metadata_files = glob.glob(f"{backtest_logs_path}/**/worker_*_metadata.json", recursive=True)
     trade_files = glob.glob(f"{backtest_logs_path}/**/worker_*_trade.parquet", recursive=True)
 
+    schema = {
+        "id": pl.Int64,
+        "strategy_name": pl.Utf8,
+        "strategy_version": pl.Utf8,
+        "strategy_params": pl.Utf8,
+        "starting_cash": pl.Float64,
+        "start_time": pl.Datetime,
+        "end_time": pl.Datetime,
+        "run_error": pl.Utf8
+    }
+
     # Grab all metadata files and combine into a single dataframe
-    metadata_df = pl.concat(pl.read_json(path) for path in metadata_files)
+    metadata_df = pl.concat(pl.read_json(path, schema=schema) for path in metadata_files)
     conn.execute("CREATE OR REPLACE TABLE backtest AS SELECT * FROM metadata_df")
 
     # Read trades individually and put into database
